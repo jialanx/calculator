d3c2c5799362736b5e13bf87b511581d54d7bf03
jialanx <jialanxie@gmail.com>
2025-07-07 16:03:11 -0700

    modulartized the code, made it much more condensed and can do * / + - operators

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
-const operators = ["+", "-", "*", "/"];
-let input = "-2+20";
+const operators = ["^", "*", "/", "+", "-"];
+let input = "2^3+3";
 input = input.replaceAll(" ", "");
 
 console.log(input); 
-console.log(addAndSubtract(input));
+console.log(solve(input));
+
 
 function findInnerEquation(equation) {
     let innerequation = equation;
@@ -31,52 +32,74 @@ function findInnerEquation(equation) {
 }
  
 function solve(equation) {
-    //equation = exponent(equation);
-    equation = multiplyAndDivide(equation);
-    return addAndSubtract(equation);
+    for (let operatorIndex in operators) {
+        console.log("parsing through operator: " + operators[operatorIndex] + " -----------------------------");
+        equation = solveWithOperator(equation, operators[operatorIndex]);
+        console.log("current equation after parse: " + equation + " ---------------------------");
+    }
+    return equation;
 }
 
-// function exponent(equation) {}
-
-function addAndSubtract(equation) {
+function solveWithOperator(equation, operator) {
     let prevNum = "";
     let nextNum = "";
-    let subtraction = false;
-    let addition = false;
+    let foundOperator = false;
+    let currentEquation = "";
 
     for (let letterIndex in equation) {
+        console.log("current letter: " + equation[letterIndex]);
         // if the letter is a number, add it to prev or next number depending if it is before or after an operator
         if (!operators.includes(equation[letterIndex])) {
-            if (subtraction || addition) { 
+            if (foundOperator) { 
                 nextNum += equation[letterIndex];
+                console.log("next number: " + nextNum + foundOperator);
             } else {
                 prevNum += equation[letterIndex];
             } 
         } else if (prevNum == "") {
+            // if its an operator and there is no existing number, it probably means its negative or positive! 
+            // make if statement in case * or / shows up
             prevNum += equation[letterIndex];
         } else {
-            if (subtraction) {
-                prevNum = (Number(prevNum) - Number(nextNum));
-                subtraction = false;
-            } else if (addition) {
-                prevNum = (Number(prevNum) + Number(nextNum));
-                addition = false;
+            if (foundOperator) {
+                prevNum = calculate(prevNum, nextNum, operator);
+                console.log("calculted number of " +prevNum)
+                foundOperator = false;
+                nextNum = "";  
             } 
+            if (equation[letterIndex] == operator) {
+                foundOperator = true
+            } else {
+                currentEquation += prevNum + equation[letterIndex];
+                prevNum = ""; 
+            }
 
-                nextNum = "";
-                if (equation[letterIndex] == "-") {
-                    subtraction = true;
-                } else if (equation[letterIndex] == "+") {
-                    addition = true;
-                }
+            console.log("current equation: " + currentEquation);
         }
     }
 
-    if (subtraction) {
-        return (Number(prevNum) - Number(nextNum));
-    } else if (addition) {  
-        return (Number(prevNum) + Number(nextNum));
+    if (foundOperator) {
+        return currentEquation + calculate(prevNum, nextNum, operator);
+    } 
+    if (!currentEquation) {
+        return equation;
     }
-    return prevNum;
+    return currentEquation + prevNum;
+}
 
+function calculate(numOne, numTwo, operator) {
+    numOne = Number(numOne); 
+    numTwo = Number(numTwo);
+    
+    if (operator == "*") return numOne * numTwo;
+    if (operator == "/") return numOne / numTwo;
+    if (operator == "+") return numOne + numTwo;
+    if (operator == "-") return numOne - numTwo;
+    if (operator == "^") {
+        let value = numOne;
+        for (let i = 1; i < numTwo; i++) {
+            value *= numOne;
+        }
+        return value;
+    }
 }
