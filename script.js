a6e2a2599e9a7997052bf29378e7fdbd0672897e
jialanx <jialanxie@gmail.com>
2025-07-08 02:36:28 -0700

    error checking - checks if the inputted equation is valid

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-
-const operators = ["^", "*", "/", "+", "-"];
-let input = "(2 * (1+ 3) + 2 * (-20+3))-74^3"; 
+const operators = ["^", "*", "/", "+", "-"]; // operators in order of PEMDAS
+let input = "3abc"; 
 input = input.replaceAll(" ", "");
 
-console.log(input); 
-console.log(findInnerEquation(input));
+console.log(input); // original equation
 
+let invalidEquation = check(input);
+
+if (!invalidEquation) {
+    console.log(findInnerEquation(input)); // solves equation
+} else {
+    console.log(invalidEquation);
+}
+
+// checks if the equation is valid
+function check(equation) {
+    const acceptableValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "(", ")", "^", "*", "/", "+", "-"];
+    const illegalBeginning = ["^", "*", "/"];
+    let prevLetter;
+    
+    if (illegalBeginning.includes(equation[0])) return "Do not begin with an operator!";
+
+    for (let letterIndex in equation) {
+        if (operators.includes(prevLetter) && operators.includes(equation[letterIndex])) return "Invalid operator placement";
+        if (!acceptableValues.includes(equation[letterIndex])) return "Your equation includes invalid characters";
+        if (operators.includes(equation[equation.length-1])) return "Your equation ends with an operator";
+
+        prevLetter = equation[letterIndex];
+    }
+    return "";
+}
+
+// searches the equation for parenthesis, uses recursion to solve each parenthesis
 function findInnerEquation(equation) {
     let newEquation = ""; 
-    
+
     for (let letterIndex in equation) { 
+
+        // if closing parenthesis, it will solve the equation and add together the string after the closing parenthesis
         if (equation[letterIndex] == ")") {
-            console.log("solving " + newEquation+ " this equals: " + solve(newEquation) + " adding: " + equation.substring(Number(letterIndex)+1, equation.length) + " which results in: " + solve(newEquation) + equation.substring(Number(letterIndex)+1, equation.length));
             return solve(newEquation) + equation.substring(Number(letterIndex)+1, equation.length);
 
+        // if it is an open parenthesis it will add the current parsing progress to the result of value within parenthesis
         } else if (equation[letterIndex] == "(") {
             newEquation += findInnerEquation(equation.substring(Number(letterIndex)+1, equation.length));
-            console.log("current equation: " + newEquation);
+
+            // if we are still in parenthesis it will rerun the function to find the closing parenthesis
             while (newEquation.includes(")")) {
                 newEquation = findInnerEquation(newEquation);
             } 
             break;
+        
+        // if we are not in a parenthesis it will add the letter to the equation we have
         } else {
             newEquation += equation[letterIndex]; 
         } 
     }   
+
+    // function doesn't solve unless it is an ending parenthesis
+    // last solve in case there is not a closing parenthesis for the equation
     return solve(newEquation);
 }
  
+// runs through each operator in PEMDAS
 function solve(equation) {
     for (let operatorIndex in operators) {
         equation = solveWithOperator(equation, operators[operatorIndex]);
@@ -35,6 +69,7 @@ function solve(equation) {
     return equation;
 }
 
+// searches for the specified operator and calculates the number before and after it
 function solveWithOperator(equation, operator) {
     let prevNum = "";
     let nextNum = ""; 
@@ -42,6 +77,7 @@ function solveWithOperator(equation, operator) {
     let currentEquation = ""; 
 
     for (let letterIndex in equation) {
+
         // if the letter is a number, add it to prev or next number depending if it is before or after an operator
         if (!operators.includes(equation[letterIndex])) {
             if (foundOperator) { 
@@ -49,40 +85,42 @@ function solveWithOperator(equation, operator) {
             } else {
                 prevNum += equation[letterIndex];
             } 
-        } else if (prevNum == "") {
-            // if its an operator and there is no existing number, it probably means its negative or positive! 
+        } else if (prevNum == "") { // if it starts with an operator, its probably negative or positive
             prevNum += equation[letterIndex];
         } else {
+            
+            // if the current letter is an operator, then the number ended and it will calculate the next and prev numbers
             if (foundOperator) {
-                if (nextNum == "") {
-                    console.log("uh oh! next number is blank, i am currently looking at: " + equation[letterIndex]);
+                if (nextNum == "") { // no existing number means its probably negative or positive
                     nextNum += equation[letterIndex];
                 } else {
-                    prevNum = calculate(prevNum, nextNum, operator);
+                    prevNum = calculate(prevNum, nextNum, operator); 
                     foundOperator = false;
                     nextNum = ""; 
                 } 
             }  
  
-            if (equation[letterIndex] == operator) {
+            if (equation[letterIndex] == operator) { 
                 foundOperator = true
-            } else if (!operators.includes(nextNum)) {
+            } else if (!operators.includes(nextNum)) { // won't add the value to the equation because the negative is already accounted for within nextNum
                 currentEquation += prevNum + equation[letterIndex];
                 prevNum = "";   
             }
 
-        }
+        } 
     }
 
-    if (foundOperator) {
+
+    if (foundOperator) { // checks if it was mid-equation looking for the number after operator
         return currentEquation + calculate(prevNum, nextNum, operator);
     } 
-    if (!currentEquation) {
+    if (!currentEquation) { // checks if currentEquation is empty
         return equation;
     }
-    return currentEquation + prevNum;
+    return currentEquation + prevNum; // if there was no operator, return the equation and the number we were checking.
 }
 
+// calculates number with given operator
 function calculate(numOne, numTwo, operator) {
     numOne = Number(numOne); 
     numTwo = Number(numTwo);
@@ -92,7 +130,7 @@ function calculate(numOne, numTwo, operator) {
     if (operator == "+") return numOne + numTwo;
     if (operator == "-") return numOne - numTwo;
     if (operator == "^") {
-        let value = numOne;
+        let value = numOne; 
         for (let i = 1; i < numTwo; i++) {
             value *= numOne;
         }
